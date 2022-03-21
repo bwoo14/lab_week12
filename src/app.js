6b085cae39a23cf50a739c360f4fb9d8ccddba14
brandon_w laptop <bwoo14@my.bcit.ca>
2022-03-21 10:52:22 -0700

    Rewrite scholarship code
    Rewrite scholarship code so that it utilizes functions

--- src/app.js
+++ src/app.js
@@ -4,3 +4,39 @@ function log_element() {
 }
 
 log_element()
+
+
+// Part 3
+
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+  
+function scholarship_list(list_students) {
+    const studentsScholarship = []
+    for (const student of list_students) {
+        const average = average_grades(student)
+        if (is_scholarship(average)){
+            studentsScholarship.push({name: student.name, gradeAvg: average})
+        }
+    }
+    return studentsScholarship
+}
+
+function average_grades(student) {
+    let acc = 0
+        for (const grade of student.grades){ // Sum student's grades
+        acc += grade
+        }
+    const average = acc/student.grades.length
+    return average
+}
+
+function is_scholarship(average) {
+    return average > 80
+}
+
+console.log(scholarship_list(students))
